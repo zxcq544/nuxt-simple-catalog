4069ff53aeba86c1eb71ffd80cdc0b93e1381802
zxcq544 <zxcq544@yandex.ru>
2023-12-20 16:38:24 +0300

    add footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { ref } from "vue";
+// import { ref } from "vue";
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
 
-const count: Ref<number> = ref(0);
-let a: number = 10;
+// const count: Ref<number> = ref(0);
+// let a: number = 10;
 </script>
 <template>
-    test variable
-    {{ a }}
-    {{ count }}
+    <Header />
+    <Footer />
 </template>
