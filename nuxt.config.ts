fc165fdb2dfebbbcf4260b5f38b4cb1cd9786127
zxcq544 <zxcq544@yandex.ru>
2023-12-20 17:04:21 +0300

    add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // modules: ["nuxt-svgo"],
+    css: ['~/assets/css/styles.css'],
     devtools: { enabled: true },
 });
